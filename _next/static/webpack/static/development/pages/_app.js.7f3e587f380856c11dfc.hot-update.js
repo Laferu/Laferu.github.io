f499b0b01e4dbcf65db17324b9de6c86ef0ce86a
Laferu <rp.araujo2@gmail.com>
2020-03-29 12:18:02 -0300

    Updates

---
+++ _next/static/webpack/static/development/pages/_app.js.7f3e587f380856c11dfc.hot-update.js
@@ -0,0 +1,83 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./utils/styles.js":
+/*!*************************!*\
+  !*** ./utils/styles.js ***!
+  \*************************/
+/*! exports provided: StyledRoot, StyledBodyScroll, StyledMain, StyledSpace, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StyledRoot", function() { return StyledRoot; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StyledBodyScroll", function() { return StyledBodyScroll; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StyledMain", function() { return StyledMain; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "StyledSpace", function() { return StyledSpace; });
+/* harmony import */ var _babel_runtime_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var react_perfect_scrollbar__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-perfect-scrollbar */ "./node_modules/react-perfect-scrollbar/lib/index.js");
+/* harmony import */ var react_perfect_scrollbar__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_perfect_scrollbar__WEBPACK_IMPORTED_MODULE_2__);
+
+
+function _templateObject5() {
+  var data = Object(_babel_runtime_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  * {\n    box-sizing: border-box;\n  }\n\n  html {\n    scroll-behavior: smooth;\n    height: 100vh;\n  }\n\n  @media screen and (prefers-reduced-motion: reduce) {\n    html {\n      scroll-behavior: auto;\n    }\n  }\n\n  html {\n    font-size: 12px;\n\n    @media screen and (min-width: 425px) {\n      font-size: 16px;\n    }\n  }\n\n  body {\n    background: #f7f9fb;\n    font-family: 'Roboto', sans-serif;\n    margin: 0;\n    padding: 0;\n    display: flex;\n    height: 100vh;\n    color: #454545;\n  }\n\n  #__next {\n    display: flex;\n    flex: 1;\n    flex-direction: column;\n  }\n\n  .container {\n    margin: 0 auto;\n    padding: 0 10px;\n    width: 100%;\n\n    @media screen and (min-width: 320px) {\n      padding: 0 10px;\n    }\n\n    @media screen and (min-width: 320px) {\n      padding: 0 30px;\n    }\n\n    @media screen and (min-width: 768px) {\n      padding: 0 50px;\n    }\n\n    @media screen and (min-width: 979px) {\n      padding: 0;\n      width: 900px;\n    }\n\n    @media screen and (min-width: 980px) {\n      width: 900px;\n    }\n\n    @media screen and (min-width: 1024px) {\n      width: 980px;\n    }\n\n    @media screen and (min-width: 1440px) {\n      width: 1024px;\n    }\n  }\n\n  .nav-toggle-show {\n    &&{\n      height: 145px;\n    }\n  }\n\n  a {\n    transition: color .2s;\n    :link, :visited {\n      text-decoration: none;\n      color: inherit;\n    }\n\n    :hover, :active {\n      color: #a7a9ac;\n    }\n  }\n"]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = Object(_babel_runtime_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  width: 100%;\n  height: 50px;\n"]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = Object(_babel_runtime_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  flex: 1 0 auto;\n  /* background-color: red; */\n"]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = Object(_babel_runtime_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  display: flex;\n  flex-direction: column;\n  flex: 1;\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = Object(_babel_runtime_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  display: flex;\n  flex: 1;\n  flex-direction: column;\n  max-height: 100vh;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+
+
+var StyledRoot = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject());
+var StyledBodyScroll = Object(styled_components__WEBPACK_IMPORTED_MODULE_1__["default"])(react_perfect_scrollbar__WEBPACK_IMPORTED_MODULE_2___default.a)(_templateObject2());
+var StyledMain = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].main(_templateObject3());
+var StyledSpace = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject4());
+/* harmony default export */ __webpack_exports__["default"] = (Object(styled_components__WEBPACK_IMPORTED_MODULE_1__["createGlobalStyle"])(_templateObject5()));
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.7f3e587f380856c11dfc.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.e150fe12574d461bbf36.hot-update.js
@@ -0,0 +1,233 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Header/index.js":
+/*!************************************!*\
+  !*** ./components/Header/index.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _styles__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles */ "./components/Header/styles.js");
+var _this = undefined,
+    _jsxFileName = "/home/laferu/projetos/rafael-araujo/components/Header/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+var Header = function Header() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(false),
+      toggleMenu = _useState[0],
+      setToggleMenu = _useState[1];
+
+  return __jsx("div", {
+    className: "container-fluid",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11,
+      columnNumber: 5
+    }
+  }, __jsx(_styles__WEBPACK_IMPORTED_MODULE_2__["StyledHeader"], {
+    className: "container ".concat(toggleMenu && 'nav-toggle-show'),
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12,
+      columnNumber: 7
+    }
+  }, __jsx("div", {
+    className: "title",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13,
+      columnNumber: 9
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14,
+      columnNumber: 11
+    }
+  }, "Rafael Araujo"), __jsx("h2", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15,
+      columnNumber: 11
+    }
+  }, "Desenvolvedor Front-End")), __jsx("nav", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17,
+      columnNumber: 9
+    }
+  }, __jsx("ul", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18,
+      columnNumber: 11
+    }
+  }, __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19,
+      columnNumber: 13
+    }
+  }, __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20,
+      columnNumber: 15
+    }
+  }, __jsx("a", {
+    title: "In\xEDcio",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21,
+      columnNumber: 17
+    }
+  }, "In\xEDcio"))), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24,
+      columnNumber: 13
+    }
+  }, "Perfil"), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25,
+      columnNumber: 13
+    }
+  }, "Projetos")), __jsx("ul", {
+    className: toggleMenu ? 'nav-show' : 'nav-hidden',
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27,
+      columnNumber: 11
+    }
+  }, __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28,
+      columnNumber: 13
+    }
+  }, "React"), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29,
+      columnNumber: 13
+    }
+  }, "React Native"), __jsx("li", {
+    onClick: function onClick() {
+      return setToggleMenu(true);
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30,
+      columnNumber: 13
+    }
+  }, "Ver mais...")), __jsx("ul", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32,
+      columnNumber: 11
+    }
+  }, __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33,
+      columnNumber: 13
+    }
+  }, "React"), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34,
+      columnNumber: 13
+    }
+  }, "React Native"), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35,
+      columnNumber: 13
+    }
+  }, "Flutter")), __jsx("ul", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37,
+      columnNumber: 11
+    }
+  }, __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 38,
+      columnNumber: 13
+    }
+  }, "PHP"), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39,
+      columnNumber: 13
+    }
+  }, "Github"), __jsx("li", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 40,
+      columnNumber: 13
+    }
+  }, "Linkedin")), __jsx("ul", {
+    className: toggleMenu ? 'nav-hidden' : 'nav-show',
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42,
+      columnNumber: 11
+    }
+  }, __jsx("li", {
+    onClick: function onClick() {
+      return setToggleMenu(false);
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43,
+      columnNumber: 13
+    }
+  }, "Recolher...")))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Header);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.e150fe12574d461bbf36.hot-update.js.map
